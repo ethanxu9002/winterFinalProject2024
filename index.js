fba122b0af71e8228bd90ef308891a7f113e1d2f
ethanxu9002@gmail.com <ethanxu9002@gmail.com>
2024-12-05 15:33:44 -0800

    started work on connect 4

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let displayList = []
-let width = 20
-let height = 10
+let width = 7
+let height = 6
+
+let everything = []
+let columnMax = []
+
+let setupAssist = []
+
+let pendPlace = 0
 
 /*█
 document.addEventListener("keypress", function(event) {
@@ -12,15 +19,36 @@ document.addEventListener("keypress", function(event) {
 
 let display = document.getElementById("display-El")
 
+
 function setup() {
-    for (let step = 0; step < width*height; step++) {
-        displayList.push(".")
+    for (let a = 0; a < width*height; a++) {
+        displayList.push(". ")
     }
+    for (let b = 0; b <7; b++) {
+        displayList.push((b+1)+" ")
+    }
+    for (let c = 0; c < width; c++) {
+        for (let d = 0; d < 6; d++) {
+            setupAssist.push(0)
+        }
+        everything.push(setupAssist)
+        setupAssist = []
+    }
+
+    for (let e = 0; e < width; e++) {
+        columnMax.push(0)
+    }
+}
+
+function placeX() {
+    pendPlace=prompt("Which row?")
+    /*columnMax[pendPlace - 1] <- come back to this*/
+
 }
 
 setup()
 
-display.style.width=width+"ch"
-display.innerText = displayList.join("")
+display.style.width=(width*2)+"ch"
+display.innerText = displayList.join("") + columnMax
 console.log(screen)
 
