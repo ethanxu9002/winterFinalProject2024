fc189a601eee7d7056f58bf6bb263dd82a5719f7
ethanxu9002@gmail.com <ethanxu9002@gmail.com>
2024-12-10 15:37:33 -0800

    YES I DID IT I MADE CONNECT 4

--- index.js
+++ index.js
@@ -1,79 +1,86 @@
-let displayList = []
-let width = 7
-let height = 6
+let columnMaxes = [] //contains the highest placed token per column
+let boardState = [] //contains all the tokens in a 2d array
+let turn = 1 //dictates whos turn it is, X=1, O=2 
 
-let everything = []
-let columnMax = []
+let debug = document.getElementById("debug")
+let turnDisplay = document.getElementById("turn-display")
 
-let setupAssist = []
-
-let pendPlace = 0
-
-/*█
-document.addEventListener("keypress", function(event) {
-  if (event.keyCode == 13) {
-    alert('hi.');
-  }
-});
-*/
-
-let display = document.getElementById("display-El")
-
+let button1 = document.getElementById("b1")
+let button2 = document.getElementById("b2")
+let button3 = document.getElementById("b3")
+let button4 = document.getElementById("b4")
+let button5 = document.getElementById("b5")
+let button6 = document.getElementById("b6")
 
 function setup() {
-    for (let a = 0; a < width*height; a++) {
-        displayList.push(". ")
-    }
-    for (let b = 0; b <7; b++) {
-        displayList.push((b+1)+" ")
-    }
-    for (let c = 0; c < width; c++) {
-        for (let d = 0; d < 6; d++) {
-            setupAssist.push(0)
+    let setupAssist = []
+    //sets up boardState by setting up 2d array, [[column][column]...]
+    for (let a = 0; a < 7; a++) {
+        for (let b = 0; b < 6; b++) {
+            setupAssist.push(".")
         }
-        everything.push(setupAssist)
-        setupAssist = []
+    boardState.push(setupAssist)
+    setupAssist=[]
     }
-
-    for (let e = 0; e < width; e++) {
-        columnMax.push(0)
+    //sets up columnMaxes, just a 1d arrray
+    for (let c = 0; c < 7; c++) {
+        columnMaxes.push(0)
     }
 }
 
-function isNotZero(value) {
-    return value != 0
-}
-
-function placeX() {
-    pendPlace=prompt("Which row?")
-    if (columnMax[pendPlace - 1] > height) {
-        //pass
-    } else if (pendPlace < width) {
-        //pass
-    } else if (pendPlace > width) {
+function place(row) {
+    //checks if column is full
+    if (columnMaxes[row] >= 6) {
+        //column is full
         //pass
     } else {
-        everything[pendPlace].splice(columnMax[pendPlace], 1, "X")
-        columnMax[pendPlace]++
+        //column is not full, placing token:
+        let currentColumn = boardState[row]
+        if (turn == 1) { //its X's turn
+            currentColumn.splice(columnMaxes[row], 1, "X")
+            boardState.splice(row, 1, currentColumn)
+        } else if (turn == 2) { //its's O's turn
+            currentColumn.splice(columnMaxes[row], 1, "O")
+            boardState.splice(row, 1, currentColumn)
+        }
+        currentColumn = []
+        let newColumnMax = columnMaxes[row]
+        columnMaxes.splice(row, 1, newColumnMax + 1)
+        
+        turn = 3 - turn
+        //displays current turn
+        if (turn == 1) {
+            turnDisplay.innerText = "X is placing"
+        } else if (turn == 2) {
+            turnDisplay.innerText = "O is placing"
+        }
     }
+    button1.innerText = boardState[0].toReversed().join("")
+    button2.innerText = boardState[1].toReversed().join("")
+    button3.innerText = boardState[2].toReversed().join("")
+    button4.innerText = boardState[3].toReversed().join("")
+    button5.innerText = boardState[4].toReversed().join("")
+    button6.innerText = boardState[5].toReversed().join("")
+
+    //debug.innerText = boardState + "|" + columnMaxes
 }
-function placeO() {
-    pendPlace=prompt("Which row?")
-    if (columnMax[pendPlace - 1] > height) {
-        //pass
-    } else if (pendPlace < width) {
-        //pass
-    } else if (pendPlace > width) {
-        //pass
-    } else {
-        everything[pendPlace].splice(columnMax[pendPlace], 1, "O")
-        columnMax[pendPlace]++
+function switchTurn() {
+    turn = 3 - turn
+    if (turn == 1) {
+        turnDisplay.innerText = "X is placing"
+    } else if (turn == 2) {
+        turnDisplay.innerText = "O is placing"
     }
 }
 
 setup()
+//debug help tool
+//debug.innerText = boardState + "|" + columnMaxes
 
-display.style.width=(width*2)+"ch"
-display.innerText = displayList.join("") + columnMax
-console.log(screen)
+//displays current turn
+if (turn = 1) {
+    turnDisplay.innerText = "X is placing"
+} else if (turn = 2) {
+    turnDisplay.innerText = "O is placing"
+}
 
